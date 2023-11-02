44bc432a636a9d8e49ce2397e848f1c502e4b577
MichaelGrinstead <mianclgr@gmail.com>
2023-11-02 12:23:18 +1000

    feat: add Membership contract

--- contracts/moda-contracts/src/Membership.sol
+++ contracts/moda-contracts/src/Membership.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
 
+import "@openzeppelin/contracts-upgradeable/access/manager/AccessManagedUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "./interfaces/IMemberShip.sol";
+
+contract Membership is IMembership {
+    /// State Variables
+
+    bytes32 private _merkleRoot;
+
+    /// External Functions
+
+    function initialize(
+        address modaAccess,
+        bytes32 merkleRoot
+    ) external initializer {
+        __AccessManaged_init(modaAccess);
+        _merkleRoot = merkleRoot;
+    }
+
+    /**
+     * @inheritdoc IAccessGate
+     */
+    function updateMerkleRoot(bytes32 merkleRoot) external restricted {
+        _merkleRoot = merkleRoot;
+    }
+
+    /**
+     * @inheritdoc IAccessGate
+     */
+    function verifyUser(
+        bytes32[] memory proof,
+        address user
+    ) external view returns (bool) {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(user))));
+        return MerkleProof.verify(proof, _merkleRoot, leaf);
+    }
+}
