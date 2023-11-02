97bc1836a32f226f69a2c69730ebda4b9bf278ed
MichaelGrinstead <mianclgr@gmail.com>
2023-11-02 13:09:12 +1000

    feat: add test for moda registry

---
+++ contracts/moda-contracts/test/ModaRegistry.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ModaRegistry} from "../src/ModaRegistry.sol";
+
+contract ModaRegistryTest is Test {}
