6daeb339717a175220df84afcb89659a3899fe2d
MichaelGrinstead <mianclgr@gmail.com>
2023-11-07 22:31:15 +1000

    test: add tests for full coverage

--- contracts/moda-contracts/test/ModaRegistry.t.sol
+++ contracts/moda-contracts/test/ModaRegistry.t.sol
@@ -12,14 +12,10 @@ contract ModaRegistryTest is Test {
 
     address public user = address(0x1);
     address public catalogAddress = address(0x2);
-    address public managerOne = address(0x3);
-    address public managerTwo = address(0x4);
     address public treasuryAddress = address(0x5);
 
     string public catalogName = "Drop";
     uint256 public dropIndex = 0;
-
-    address[] public managers = new address[](2);
 
     function setUp() public {
         membership = new Membership();
@@ -39,10 +35,28 @@ contract ModaRegistryTest is Test {
         assertTrue(isMember);
     }
 
-    function test_setCatalogMembership() public {
+    function test_isNotMember() public {
         membershipSetUp();
+        address notMember = address(0x3);
+        bool isMember = modaRegistry.isMember(dropIndex, notMember);
+        assertFalse(isMember);
+    }
+
+    function test_changeCatalogMembership() public {
+        membershipSetUp();
+        address newMembership = address(0x4);
+        modaRegistry.setCatalogMembership(dropIndex, newMembership);
         IModaRegistry.Catalog memory catalog = modaRegistry.getCatalogInfo(dropIndex);
-        assertEq(catalog.membership, address(membership));
+        assertEq(catalog.membership, newMembership);
+    }
+
+    /// Membership reverts
+
+    function test_RevertWhen_changeCatalogMembershipWithZeroAddress() public {
+        membershipSetUp();
+        address newMembership = address(0x0);
+        vm.expectRevert(ModaRegistry.AddressCannotBeZero.selector);
+        modaRegistry.setCatalogMembership(dropIndex, newMembership);
     }
 
     /// Catalog registration
@@ -74,41 +88,102 @@ contract ModaRegistryTest is Test {
         assertEq(count, 1);
     }
 
+    /// Catalog reverts
+
+    function test_RevertWhen_registerCatalogWithZeroAddress() public {
+        vm.expectRevert(ModaRegistry.AddressCannotBeZero.selector);
+        modaRegistry.registerCatalog(catalogName, address(0), address(membership));
+    }
+
+    function test_RevertWhen_catalogAlreadyRegistered() public {
+        registerCatalogSetUp();
+        vm.expectRevert(ModaRegistry.CatalogAlreadyRegistered.selector);
+        modaRegistry.registerCatalog(catalogName, catalogAddress, address(membership));
+    }
+
+    function test_RevertWhen_unregisterAlreadyUnregisteredCatalog() public {
+        registerCatalogSetUp();
+        modaRegistry.unregisterCatalog(dropIndex);
+        vm.expectRevert(ModaRegistry.CatalogNotRegistered.selector);
+        modaRegistry.unregisterCatalog(dropIndex);
+    }
+
     /// Artist Management
 
+    struct ArtistManagementData {
+        address[] managers;
+        address managerOne;
+        address managerTwo;
+    }
+
+    ArtistManagementData artistManagementData;
+
     function artistManagementSetUp() public {
-        managers[0] = managerOne;
-        managers[1] = managerTwo;
+        artistManagementData.managerOne = address(0x3);
+        artistManagementData.managerTwo = address(0x4);
+        artistManagementData.managers = new address[](2);
+        artistManagementData.managers[0] = artistManagementData.managerOne;
+        artistManagementData.managers[1] = artistManagementData.managerTwo;
         vm.startPrank(user);
-        modaRegistry.addManagers(managers);
+        modaRegistry.addManagers(artistManagementData.managers);
         vm.stopPrank();
     }
 
     function test_addManagers() public {
         artistManagementSetUp();
-        bool isManagerOne = modaRegistry.isManager(user, managerOne);
-        bool isManagerTwo = modaRegistry.isManager(user, managerTwo);
+        bool isManagerOne = modaRegistry.isManager(user, artistManagementData.managerOne);
+        bool isManagerTwo = modaRegistry.isManager(user, artistManagementData.managerTwo);
         assertTrue(isManagerOne);
         assertTrue(isManagerTwo);
         uint256 managerCount = modaRegistry.getManagerCount(user);
         assertEq(managerCount, 2);
         address managerOneAddress = modaRegistry.getManager(user, 0);
         address managerTwoAddress = modaRegistry.getManager(user, 1);
-        assertEq(managerOneAddress, managerOne);
-        assertEq(managerTwoAddress, managerTwo);
+        assertEq(managerOneAddress, artistManagementData.managerOne);
+        assertEq(managerTwoAddress, artistManagementData.managerTwo);
     }
 
     function test_removeManagers() public {
         artistManagementSetUp();
         vm.startPrank(user);
-        modaRegistry.removeManagers(managers);
+        modaRegistry.removeManagers(artistManagementData.managers);
         vm.stopPrank();
-        bool isManagerOne = modaRegistry.isManager(user, managerOne);
-        bool isManagerTwo = modaRegistry.isManager(user, managerTwo);
+        bool isManagerOne = modaRegistry.isManager(user, artistManagementData.managerOne);
+        bool isManagerTwo = modaRegistry.isManager(user, artistManagementData.managerTwo);
         assertFalse(isManagerOne);
         assertFalse(isManagerTwo);
         uint256 managerCount = modaRegistry.getManagerCount(user);
         assertEq(managerCount, 0);
+    }
+
+    /// Artist Management reverts
+
+    function test_RevertWhen_addManagersWithZeroAddress() public {
+        vm.expectRevert(ModaRegistry.AddressCannotBeZero.selector);
+        address[] memory managers = new address[](1);
+        managers[0] = address(0);
+        vm.startPrank(user);
+        modaRegistry.addManagers(managers);
+        vm.stopPrank();
+    }
+
+    function test_RevertWhen_removeManagersWithZeroAddress() public {
+        vm.expectRevert(ModaRegistry.AddressCannotBeZero.selector);
+        address[] memory managers = new address[](1);
+        managers[0] = address(0);
+        vm.startPrank(user);
+        modaRegistry.removeManagers(managers);
+        vm.stopPrank();
+    }
+
+    function test_RevertWhen_addManagersWithDuplicateAddress() public {
+        vm.expectRevert(ModaRegistry.DuplicateAddress.selector);
+        address[] memory managers = new address[](2);
+        managers[0] = address(0x3);
+        managers[1] = address(0x3);
+        vm.startPrank(user);
+        modaRegistry.addManagers(managers);
+        vm.stopPrank();
     }
 
     /// Treasury
@@ -123,5 +198,12 @@ contract ModaRegistryTest is Test {
         (address treasury, uint256 fee) = modaRegistry.getTreasury();
         assertEq(treasury, treasuryAddress);
         assertEq(fee, 1000);
+    }
+
+    //Treasury reverts
+
+    function test_RevertWhen_setTreasuryWithZeroAddress() public {
+        vm.expectRevert(ModaRegistry.AddressCannotBeZero.selector);
+        modaRegistry.setTreasury(address(0));
     }
 }
