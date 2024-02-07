c7c5fb4ad785adfba5478bfcc2b9ee96a76310bc
MichaelGrinstead <michael.a.grinstead@gmail.com>
2024-02-07 11:32:01 +1000

    refactor: generate wagmi hooks for drop-sdk

---
+++ packages/drop-sdk/src/constants.ts
@@ -0,0 +1,24 @@
+export const RegistryAddresses = {
+  mumbai: '0x1',
+  mainnet: '0x2',
+};
+
+export const CatalogFactoryAddresses = {
+  mumbai: '0x3',
+  mainnet: '0x4',
+};
+
+export const CatalogAddresses = {
+  mumbai: '0x5',
+  mainnet: '0x6',
+};
+
+export const ReleasesFactoryAddresses = {
+  mumbai: '0x7',
+  mainnet: '0x8',
+};
+
+export const ReleasesAddresses = {
+  mumbai: '0x9',
+  mainnet: '0x10',
+};
